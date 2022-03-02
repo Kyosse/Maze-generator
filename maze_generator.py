2ce0bdc7ce8c761a0768c0c99c1fbd7c7fac1383
Kyosse <Kyogre85@gmail.com>
2022-03-02 19:17:45 +0100

    Update maze_generator.py

--- maze_generator.py
+++ maze_generator.py
@@ -26,6 +26,17 @@ class Pile():
 
 
 class Case:
+    """Class Case qui permet de contenir différentes valeurs
+
+    posn(int): valeur de la position en x
+    posm(int): valeur de la position en y
+    n(int): représente le 'mur' dans la direction nord (1 = mur | 0 = vide)
+    o(int): ''
+    s(int): ''
+    e(int): ''
+    exploree(int): permet de reconnaître si la case à été modifiée
+    """
+
     def __init__(self, Posn, Posm, Nord=1, Ouest=1, Sud=1, Est=1, Exploree=0):
         self.posn = Posn
         self.posm = Posm
@@ -65,35 +76,85 @@ class Case:
     def getExploree(self):
         return self.exploree
 
+    def getPosn(self):
+        return self.posn
+
+    def getPosm(self):
+        return self.posm
+
     def __str__(self):
         str = f"Case{self.posn, self.posm, self.n, self.o, self.s, self.e}"
         return str
 
     def voisinNord(self, laby):
-        if self.posn == 0:
+        """Méthode qui renvoie le voisin, dans la direction voulu, de la case
+
+        Args:
+            laby(obj): Labyrinthe contenant toutes les cases
+
+        Returns:
+            None: La case ne comporte pas de voisin
+            Case(obj): La case de la direction voulu 
+        """
+        if self.posn == 0:  # Si la coordonnée x est égale à 0 alors la case est "collée" au bord donc elle n'a pas de voisin
             return None
         else:
             return laby[self.posn - 1][self.posm]
 
     def voisinOuest(self, laby):
-        if self.posm == 0:
+        """Méthode qui renvoie le voisin, dans la direction voulu, de la case
+
+        Args:
+            laby(obj): Labyrinthe contenant toutes les cases
+
+        Returns:
+            None: La case ne comporte pas de voisin
+            Case(obj): La case de la direction voulu 
+        """
+        if self.posm == 0:  # Si la coordonnée y est égale à 0 alors la case est "collée" au bord donc elle n'a pas de voisin
             return None
         else:
             return laby[self.posn][self.posm - 1]
 
     def voisinSud(self, laby):
-        if self.posn == (len(laby) - 1):
+        """Méthode qui renvoie le voisin, dans la direction voulu, de la case
+
+        Args:
+            laby(obj): Labyrinthe contenant toutes les cases
+
+        Returns:
+            None: La case ne comporte pas de voisin
+            Case(obj): La case de la direction voulu 
+        """
+        if self.posn == (len(laby) - 1):  # Si la coordonnée x est égale à la longeur du labyrinthe alors la case est "collée" au bord donc elle n'a pas de voisin
             return None
         else:
             return laby[self.posn + 1][self.posm]
 
     def voisinEst(self, laby):
-        if self.posm == (len(laby[self.posn]) - 1):
+        """Méthode qui renvoie le voisin, dans la direction voulu, de la case
+
+        Args:
+            laby(obj): Labyrinthe contenant toutes les cases
+
+        Returns:
+            None: La case ne comporte pas de voisin
+            Case(obj): La case de la direction voulu 
+        """
+        if self.posm == (len(laby[self.posn]) - 1):  # Si la coordonnée y est égale à la longeur du labyrinthe alors la case est "collée" au bord donc elle n'a pas de voisin
             return None
         else:
             return laby[self.posn][self.posm + 1]
 
     def voisins(self, laby):
+        """Méthode qui renvoie les voisins de la case
+
+        Args:
+            laby(obj): Class Labyrinthe contenant toutes les cases
+
+        Returns:
+            liste_voisins(list): Liste contenant les voisins de la case
+        """
         liste_voisins = []
         if self.voisinNord(laby) != None:
             liste_voisins.append(self.voisinNord(laby))
@@ -104,16 +165,6 @@ class Case:
         if self.voisinEst(laby) != None:
             liste_voisins.append(self.voisinEst(laby))
         return liste_voisins
-
-    def est_bloque(self, laby):
-        tmp = 0
-        for i in self.voisins(laby):
-            if i.getExploree() == 1:
-                tmp += 1
-        if tmp == len(self.voisins(laby)):
-            return True
-        else:
-            return False
 
 
 class Labyrinthe:
@@ -130,33 +181,28 @@ class Labyrinthe:
     def __str__(self):
         print('__' * (self.width * 2))
         for i in range(self.height):
-            up = ''
-            down = ''
+            ligne = ''
             for j in range(self.width):
-                str_up = ''
-                str_down = ''
+                str = ''
                 ouest = self.cells[i][j].getOuest()
                 nord = self.cells[i][j].getNord()
                 est = self.cells[i][j].getEst()
                 sud = self.cells[i][j].getSud()
 
                 if ouest == 1:
-                    str_down += '|'
+                    str += '|'
                 else:
-                    str_down += ' '
+                    str += ' '
                 if sud == 1:
-                    str_down += '__'
+                    str += '__'
                 else:
-                    str_down += '  '
+                    str += '  '
                 if est == 1:
-                    str_down += '|'
+                    str += '|'
                 else:
-                    str_down += ' '
-
-                # up += str_up
-                down += str_down
-            # print(up)
-            print(down)
+                    str += ' '
+                ligne += str
+            print(ligne)
         return ''
 
     def get_case(self, i, j):
@@ -170,73 +216,45 @@ class Labyrinthe:
                 temp_lab[i].append(self.cells[i][j].__str__())
         return temp_lab
 
-    def casser_murs(self, depart):
-        mur_a_casser = random.randint(1, 4)
-        if mur_a_casser == 1:
-            res = depart.voisinNord(self.cells)
-            if res == None or res.exploree == 1 and self.toutes_explorees() is False:
-                self.casser_murs(depart)
-            else:
-                depart.setNord(0)
-                res.setSud(0)
-        elif mur_a_casser == 2:
-            res = depart.voisinOuest(self.cells)
-            if res == None or res.exploree == 1 and self.toutes_explorees() is False:
-                self.casser_murs(depart)
-            else:
-                depart.setOuest(0)
-                res.setEst(0)
-        elif mur_a_casser == 3:
-            res = depart.voisinSud(self.cells)
-            if res == None or res.exploree == 1 and self.toutes_explorees() is False:
-                self.casser_murs(depart)
-            else:
-                depart.setSud(0)
-                res.setNord(0)
-        else:
-            res = depart.voisinEst(self.cells)
-            if res == None or res.exploree == 1 and self.toutes_explorees() is False:
-                self.casser_murs(depart)
-            else:
-                depart.setEst(0)
-                res.setOuest(0)
+    def connect(self, case_depart, case_voisin):
+        x = case_voisin.getPosn() - case_depart.getPosn()
+        y = case_voisin.getPosm() - case_depart.getPosm()
 
-    def toutes_explorees(self):
-        for tab in self.cells:
-            for case in tab:
-                if case.getExploree() == 0:
-                    return False
-        return True
+        if x == -1 and y == 0:  # Voisin nord
+            case_depart.setNord(0)
+            case_voisin.setSud(0)
+        elif x == 1 and y == 0:  # Voisin sud
+            case_depart.setSud(0)
+            case_voisin.setNord(0)
+        elif x == 0 and y == -1:  # Voisin ouest
+            case_depart.setOuest(0)
+            case_voisin.setEst(0)
+        else:                      # Voisin est
+            case_depart.setEst(0)
+            case_voisin.setOuest(0)
 
 
 def creation_laby(laby, depart):
     visites = []
-    fermes = []
     p = Pile()
     visites.append(depart)
     p.empiler(depart)
     while not p.est_vide():
         temp = p.sommet_pile()
         voisin = []
-
         for i in temp.voisins(laby.cells):
             if i not in visites and p.est_present(i) is False:
                 voisin.append(i)
-
         if voisin != []:
             v = random.choice(voisin)
-            if v.est_bloque(laby.cells) == True:
-                pass
-            else:
-                laby.casser_murs(v)
+            laby.connect(temp, v)
             visites.append(v)
             v.setExploree(1)
             p.empiler(v)
         else:
-            fermes.append(temp)
             p.depiler()
 
 
-laby = Labyrinthe(10, 10)
+laby = Labyrinthe(10, 5)
 print(laby)
 print(laby.affiche())
